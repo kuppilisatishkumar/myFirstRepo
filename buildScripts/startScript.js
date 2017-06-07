8fb7465f963ace9effe710d879b55a4e3c77ff5f
kuppilisatishkumar <kuppilisatishkumar1@gmail.com>
2017-06-08 01:06:47 +0530

    added a w3 template to index2.html

--- buildScripts/startScript.js
+++ buildScripts/startScript.js
@@ -1,3 +1,4 @@
 import chalk from 'chalk'; //all the other files are written ----
 // in ES5 but this line is written in ES6 and transpiled to version ES5 using babel
 console.log(chalk.green('Appliation is getting started'));
+
